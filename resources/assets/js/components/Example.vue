6453c2eea794c28a9787fd14e7f30281ee9a3a2c
hemnys <hemnys25@gmail.com>
2017-07-27 09:34:16 -0400

    Creating a component for the select2 library

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -5,20 +5,30 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Example Component</div>
                 <div class="panel-body">
-
-                    <select name="" id="" class="form-control">
-                        <option value="1">prueba</option>
-                    </select>
+                    <select2 :options="options" v-model="selected">
+                        <option disabled value="0">Select one</option>
+                    </select2>
                 </div>
             </div>
         </div>
     </div>
 </div>
 </template>
+
 <script>
+
 export default {
     mounted() {
         console.log('Component mounted.')
+    },
+    data: function(){
+        return {
+            selected: 2,
+            options:[
+                {id:1,text:'hello'},
+                {id:2,text:'hello 2'},
+            ]
+        }
     }
 }
 </script>
